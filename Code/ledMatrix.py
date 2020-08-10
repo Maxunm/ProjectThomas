9235f28f6bf0f4e2fc4b39ed582712669bd85bb9
= <dstewart@knightglobal.com>
2020-08-10 08:12:38 -0400

    Missing parenthese fixed

--- Code/ledMatrix.py
+++ Code/ledMatrix.py
@@ -29,7 +29,8 @@ class Panel:
         self.panelNumber = panelNumber
         self.leds = []
         for x in range(0, 16):
-            self.leds.append(LED((16*panelNumber)+x)  # Might have to look at this math later
+            # Might have to look at this math later
+            self.leds.append(LED((16*panelNumber)+x))
 
     def setPanelLights(self):
         for x in range(len(self.leds)):
@@ -38,10 +39,10 @@ class Panel:
 
 class LED:
     def __init__(self, ledNumber):
-        self.ledNumber=ledNumber
-        self.r=random.randrange(0, 255)
-        self.g=random.randrange(0, 255)
-        self.b=random.randrange(0, 255)
+        self.ledNumber = ledNumber
+        self.r = random.randrange(0, 255)
+        self.g = random.randrange(0, 255)
+        self.b = random.randrange(0, 255)
 
     def setLedColor(self):  # Implement later with actual WS2813 lib function
         # self.ledNumber.set(r,g,b)
