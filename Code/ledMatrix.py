97e1c138be8415db3e8a8dcd1cc06dc5298a1dbf
= <dstewart@knightglobal.com>
2020-08-10 07:55:02 -0400

    Cleaned up ledMatrix.py

--- Code/ledMatrix.py
+++ Code/ledMatrix.py
@@ -1,33 +1,19 @@
 import json
 import random
 
+
 class Animation:
     def __init__(self):
         self.matrices = []
         for x in range(0, 50):
             self.matrices.append(Matrix())
 
-    # def __str__(self):
-    #     returnArray = []
-    #     for x in range(len(self.panels)):
-    #         returnArray.append(str(self.panels[x]))
-    #     return str(returnArray)
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
 
 class Matrix:
     def __init__(self):
         self.panels = []
         for x in range(0, 20):
             self.panels.append(Panel())
-
-    # def __str__(self):
-    #     returnArray = []
-    #     for x in range(len(self.panels)):
-    #         returnArray.append(str(self.panels[x]))
-    #     return str(returnArray)
 
 
 class Panel:
@@ -36,26 +22,9 @@ class Panel:
         for x in range(0, 16):
             self.leds.append(LED())
 
-    # def __str__(self):
-    #     returnArray = []
-    #     for x in range(len(self.leds)):
-    #         returnArray.append(str(self.leds[x]))
-    #     return str(returnArray)
-
 
 class LED:
     def __init__(self):
         self.r = random.randrange(0, 255)
         self.g = random.randrange(0, 255)
         self.b = random.randrange(0, 255)
-
-    # def value(self):
-    #     return self.r, self, g, self.b
-
-    # def __str__(self):
-    #     return "r:" + str(self.r) + " g:" + str(self.g) + " b:" + str(self.b)
-
-
-# class MatrixEncoder(JSONEncoder):
-#     def default(self, o):
-#         return o.__dict__
